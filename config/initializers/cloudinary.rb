e0f54b195dd47dd42196d4e98d2d80286c3bb04c
Anatole Ayme <anatole.ayme@gmail.com>
2026-03-02 11:16:37 +0100

    ok

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,10 +3,3 @@ Cloudinary.config do |config|
   config.api_key    = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_API_SECRET']
 end
-```
-
-**4. Ajoute tes credentials dans le `.env` :**
-```
-CLOUDINARY_CLOUD_NAME=xxxx
-CLOUDINARY_API_KEY=xxxx
-CLOUDINARY_API_SECRET=xxxx
